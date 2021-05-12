c9c77aeffbfb0728a65a6ea44e48524f3fa83409
Sacha COHEN <contact@sacha-cohen.fr>
2021-05-12 06:40:46 +0200

    Initialized project : Header tags, global css

---
+++ code/assets/inc/header-tags.php
@@ -0,0 +1,12 @@
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;500;700;900&display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<link rel="icon" href="./assets/img/logo.png"/>
+<meta name="author" content="contact@sacha-cohen.fr">
+<!-- CSS -->
+<link rel="stylesheet" href="assets/css/__mixin.css?version=1.00">
+<script src="https://use.fontawesome.com/releases/v5.11.2/js/all.js" data-auto-replace-svg="nest"></script>
+<!-- JS -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="assets/js/app.js?version=1.0.0"></script>
---
+++ code/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Blogify | Accueil</title>
+        <meta name="description" content="Blogify is a personal project realized for my school fifth project.">
+        <?php include('assets/inc/header-tags.php') ?>
+    </head>
+</html>
