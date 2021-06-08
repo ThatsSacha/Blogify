fcb721e01c3010e40607e371417f55b079a261bc
Sacha COHEN <contact@sacha-cohen.fr>
2021-06-08 07:06:29 +0200

    Article class

---
+++ code/Model/Class/Article.php
@@ -0,0 +1,67 @@
+<?php
+
+class Article {
+    private string $title;
+    private string $teaser;
+    private string $content;
+    private string $cover;
+    private int  $authorId;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void {
+        $this->title = $title;
+    }
+
+    public function getTeaser(): string {
+        return $this->teaser;
+    }
+
+    public function setTeaser(string $teaser): void {
+        $this->teaser = $teaser;
+    }
+
+    public function getContent(): string {
+        return $this->content;
+    }
+
+    public function setContent(string $content): void {
+        $this->content = $content;
+    }
+
+    public function getCover(): string {
+        return $this->cover;
+    }
+
+    public function setCover(string $cover): void {
+        $this->cover = $cover;
+    }
+
+    public function getAuthorId(): int {
+        return $this->authorId;
+    }
+
+    public function setAuthorId(int $authorId): void {
+        $this->authorId = $authorId;
+    }
+
+    public function getCreatedAt(): DateTime {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void {
+        $this->updatedAt = $updatedAt;
+    }
+}
