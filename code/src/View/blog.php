817881c4fe4667b7f1d2867d89895b2ce373f8d5
Sacha COHEN <contact@sacha-cohen.fr>
2021-07-28 06:59:31 +0200

    Blog view

--- code/src/View/blog.php
+++ code/src/View/blog.php
@@ -8,7 +8,20 @@
     <body>
         <?php include('assets/inc/side-bar.php'); ?>
         <main>
-            <h1>Blog</h1>
+            <h1>Blog 📃</h1>
+            <div class="article-container">
+                <?php
+                    foreach($this->json as $article) {
+                        ?>
+                            <div class="card">
+                                <div class="cover" style="background-image: url(../../assets/img/blog/<?= $article['cover'] ?>)"></div>
+                                <h3><?= $article['title'] ?></h3>
+                                <span>Le 8 mars - Sacha COHEN</span>
+                            </div>
+                        <?php
+                    }
+                ?>
+            </div>
         </main>
     </body>
 </html>
