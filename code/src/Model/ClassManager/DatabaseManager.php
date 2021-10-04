23ad31954e96df16ac7714461c8e60d4a75896a8
Sacha COHEN <contact@sacha-cohen.fr>
2021-10-04 06:00:02 +0200

    Fixed .env

--- code/src/Model/ClassManager/DatabaseManager.php
+++ code/src/Model/ClassManager/DatabaseManager.php
@@ -10,7 +10,7 @@ use PDOStatement;
 class DatabaseManager {
     public function connect(): PDO|PDOException {
         try {
-            return new PDO('mysql:host=127.0.0.1;dbname=' . $_ENV['DB_NAME'] . ';charset=utf8', $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+            return new PDO('mysql:host='. $_ENV['DB_HOST'] .';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8', $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
         } catch(PDOException $e) {
             throw new PDOException($e->getMessage());
         }
