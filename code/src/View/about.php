fd598e5af00d4c2806c18674ad36cb5ce47a9dda
Sacha COHEN <contact@sacha-cohen.fr>
2021-10-12 05:27:35 +0200

    About page

--- code/src/View/about.php
+++ code/src/View/about.php
@@ -23,25 +23,25 @@
                 </p>
             </div>
             <div class="card second-card">
-                <h2>My name is COHEN. <em>Sacha COHEN.</em></h2>
-                <h3>I am a developer. A full-stack web developer.</h3>
+                <h2>Développeur passionné</h2>
+                <h3>Et autodidacte...</h3>
                 <p>
-                    Maîtrisant l'anglais principalement pour l'humour, les séries, les besoins professionels et mondiaux, je pratique cette langue depuis l'âge de 11 ans <small><em>(après J.C.)</em></small>.
+                    À 15 ans j'ai appris à créer des sites internet.<br/><em>WordPress</em>, <em>Wix</em>, ils me connaissent tellement bien là-bas que lorsque j'y touche aujourd'hui, leur côte en bourse est en hausse de 20% !🤪
                 </p>
             </div>
             <div class="card-container">
                 <div class="card">
-                    <h2>My name is COHEN. <em>Sacha COHEN.</em></h2>
-                    <h3>I am a developer. A full-stack web developer.</h3>
+                    <h2>Les choses sérieuses <small>(et les bugs)</small> commencent...</h2>
+                    <h3>Il faut bien se mouiller !</h3>
                     <p>
-                        Maîtrisant l'anglais principalement pour l'humour, les séries, les besoins professionels et mondiaux, je pratique cette langue depuis l'âge de 11 ans <small><em>(après J.C.)</em></small>.
+                        2018, l'année de mon Bac, je commence à apprendre à coder. <em>OpenClassrooms</em>, <em>Codecademy</em>, ils me connaissent tellement bien là-bas que... bon bref ok.😂
                     </p>
                 </div>
                 <div class="card">
-                    <h2>My name is COHEN. <em>Sacha COHEN.</em></h2>
-                    <h3>I am a developer. A full-stack web developer.</h3>
+                    <h2>Études, boulot, ???</em></h2>
+                    <h3>Error 404, fin de la phrase not found</h3>
                     <p>
-                        Maîtrisant l'anglais principalement pour l'humour, les séries, les besoins professionels et mondiaux, je pratique cette langue depuis l'âge de 11 ans <small><em>(après J.C.)</em></small>.
+                        Bac +2, stage à Paris, projets persos, embauche dans une entreprise, alternance, autoentreprise & <em>StackOverflow</em>, en résumé on est passé dans la cours des grands...
                     </p>
                 </div>
             </div>
